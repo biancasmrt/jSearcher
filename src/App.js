437ced5cf1bf80ca34c6fa91a1ae3d0a0bc278e5
Bianca <bsamrt7@gatech.edu>
2019-10-23 16:08:57 -0400

    hmm

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bianca's kickass app bitch
         </a>
       </header>
     </div>
