bf971fcad3f73156e97f7a936a562f1090412b31
Bianca <bsamrt7@gatech.edu>
2019-10-30 23:50:54 -0400

    Thanks Justin

--- src/App.js
+++ src/App.js
@@ -1,69 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import axios from 'axios';
-import logo from './logo.svg';
-import pic from './images/logo.png';
-import Refine from './components/Refine.js';
-import Display from './components/Display.js';
-import './App.css';
-
-=======
 import Header from './Header';
 import Refine from './Refine';
->>>>>>> a367a92c2a54c868236523b4d5f6ac0183154921
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Refine/>
-      <header className="App-header">
-        <h1>
-          jSearcher   
-        </h1>
-      </header>
-      
-    <body>
-      <div class="refine">
-        <h1>Refine</h1>
-        <div class="option">
-        <label for="when">Date:&nbsp; </label>
-        <input id="when" name="when" type="date" min="1964-01-01" max="2019-12-01"/>
-        <br/>
-        </div>
-        <div class="option">
-          Level of Difficulty&nbsp;
-          <select id="diffVal">
-            <option value="0">All</option>
-            <option value="200">200</option>
-            <option value="400">400</option>
-            <option value="600">600</option>
-            <option value="800">800</option>
-            <option value="1000">1000</option>
-          </select>
-        </div>
-        <div class="option">
-          <form >
-              Trivia Category&nbsp;
-            <input type="text" name="Category" />
-          </form>
-        </div>
-      </div>
-    </body>
-    <script type="application/javascript">
-      var e = document.getElementById("diffVal");
-      var diffValue = e.options[e.selectedIndex].value;
-  </script>
-
-
-=======
       <Header />
       <Refine />
       <script type="application/javascript">
         var e = document.getElementById("diffVal");
         var diffValue = e.options[e.selectedIndex].value;
       </script>
->>>>>>> a367a92c2a54c868236523b4d5f6ac0183154921
     </div>
   );
 
